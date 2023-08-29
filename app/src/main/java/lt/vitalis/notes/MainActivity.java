8ba168a68e8b2a214269b6fb1ac6ecf2d648ae83
Vitalis <vkaubrys@gmail.com>
2023-08-29 21:24:05 +0300

    Impliment Listview in MainActivity

--- app/src/main/java/lt/vitalis/notes/MainActivity.java
+++ app/src/main/java/lt/vitalis/notes/MainActivity.java
@@ -4,30 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import lt.vitalis.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        TextView myTextView = findViewById(R.id.myTextView);
+        ArrayList<String> list = new ArrayList<>();
 
-        binding.myTextView.setText("Kokia grazi diena ir as koks nuostabus");
-//        binding.myTextView.setTextSize(55);
-//        binding.myTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        List<String> newList = Arrays.asList(
+                "Pirmadienis",
+                "Antradienis",
+                "Treciadienis",
+                "Ketvirtedienis",
+                "Penktadienis",
+                "Sestadienis",
+                "Sekmadienis"
+        );
 
-//        myTextView.setText("Kokia grazi diena");
-//        myTextView.setTextSize(20);
+
+        list.addAll(newList);
+        list.addAll(newList);
+        list.addAll(newList);
+        list.addAll(newList);
+        list.addAll(newList);
+
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list
+        );
+        binding.notesListView.setAdapter(adapter);
+
+
     }
 }
