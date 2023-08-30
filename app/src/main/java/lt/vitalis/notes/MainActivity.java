c2e87185cf82501563fab43333cb958a09b7fc87
Vitalis <vkaubrys@gmail.com>
2023-08-30 19:55:54 +0300

    Add alert dialog to remove item

--- app/src/main/java/lt/vitalis/notes/MainActivity.java
+++ app/src/main/java/lt/vitalis/notes/MainActivity.java
@@ -1,18 +1,17 @@
 package lt.vitalis.notes;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import lt.vitalis.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "my_notes_main_activity";
     private ActivityMainBinding binding;
     private ArrayAdapter<Note> adapter;
     private ArrayList<Note> notes;
@@ -24,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setUpListView();
-        setUpListViewClickListener();
+        setUpListViewItemClick();
+        setUpListViewItemLongClick();
     }
+
     private void setUpListView() {
         notes = new ArrayList<>();
 
@@ -40,16 +41,39 @@ public class MainActivity extends AppCompatActivity {
         );
         binding.notesListView.setAdapter(adapter);
     }
-    private void setUpListViewClickListener() {
+    private void setUpListViewItemClick() {
         binding.notesListView.setOnItemClickListener(
                 (adapterView, view, position, l) -> {
-                    Log.i(TAG,"OnListeItemClicked: " + adapterView.getItemIdAtPosition(position));
-                    Log.i(TAG,"OnListeItemClicked: " + position);
-
-
-
+                    Log.i(TAG, "OnListeItemClicked: " + adapterView.getItemIdAtPosition(position));
+                    Log.i(TAG, "OnListeItemClicked: " + position);
                 }
         );
+    }
+    private void setUpListViewItemLongClick() {
+        binding.notesListView.setOnItemLongClickListener(
+                (adapterView, view, position, l) -> {
+                    Log.i(TAG, "OnListItem_LONG_Clicked: " + adapterView.getItemAtPosition(position));
+                    Note note = (Note) adapterView.getItemAtPosition(position);
+                    showItemRemoveAlertDialog(note);
+                    return true;
+                }
+        );
+    }
+
+    private void showItemRemoveAlertDialog(Note note) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setMessage("Do you realy want to remove thid item?")
+                .setPositiveButton("Yes", (dialogInterface, i) -> {
+            removeNoteFromList(note);
+        })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private void removeNoteFromList (Note note) {
+        notes.remove(note);
+        adapter.notifyDataSetChanged();
 
     }
 }
